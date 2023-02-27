af20ffceb497884ee9469707c67538d719477de4
z3301 <98485231+z3301@users.noreply.github.com>
2023-02-27 17:06:34 -0500

    fixed image route

--- app.py
+++ app.py
@@ -134,37 +134,35 @@ def index():
 def image():
     print("GET /image")
     image_html = """
-    test
-    """
-            # <!DOCTYPE html>
-            # <html>
-            # <head>
-            #     <title>Upload Image</title>
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Upload Image</title>
                 
-            #     <style>
-            #     fieldset { margin: 0; }  
-            #     legend { font-size: 1.5em; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-weight: bold; }
-            #     input { margin: 10px; }
-            #     button { margin: 10px; }
-            #     li { font-size: 1em; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; margin: 10px; }
-            #     </style>
+                <style>
+                fieldset { margin: 0; }  
+                legend { font-size: 1.5em; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-weight: bold; }
+                input { margin: 10px; }
+                button { margin: 10px; }
+                li { font-size: 1em; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; margin: 10px; }
+                </style>
                 
-            # </head>
-            # <body>
-    #             <fieldset>
-    #                 <form method="POST" enctype="multipart/form-data" action="/upload">
-    #                 <legend>Upload Image</legend>
-    #                 <input type="file" id="file" name="form_file" accept="image/jpeg"/>
-    #                 <button>Upload</button>
-    #                 </form>
-    #             </fieldset>
-    #             <br />
-    #             <div align="right"><button onClick='document.location.href="https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://34.172.81.167:8080/"'>Log Out</button></div>
+            </head>
+            <body>
+                <fieldset>
+                    <form method="POST" enctype="multipart/form-data" action="/upload">
+                    <legend>Upload Image</legend>
+                    <input type="file" id="file" name="form_file" accept="image/jpeg"/>
+                    <button>Upload</button>
+                    </form>
+                </fieldset>
+                <br />
+                <div align="right"><button onClick='document.location.href="https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=http://34.172.81.167:8080/"'>Log Out</button></div>
             
-    #         """
+            """
             
-    # for file in list_of_files():
-    #     image_html += "<li><a href=\"/files/" + file + "\">" + file + "</a></li>"
+    for file in list_of_files():
+        image_html += "<li><a href=\"/files/" + file + "\">" + file + "</a></li>"
     
     return image_html
 
